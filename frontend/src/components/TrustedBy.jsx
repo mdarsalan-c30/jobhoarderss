95b41fee61a52f4526fbeb7224a0ce204e8deebd
mdarsalan-c30 <105696006+mdarsalan-c30@users.noreply.github.com>
2025-05-07 14:15:35 +0530

    Update TrustedBy.jsx

--- frontend/src/components/TrustedBy.jsx
+++ frontend/src/components/TrustedBy.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
 import './TrustedBy.css';
 
 const TrustedBy = () => {
   return (
-    <div className="trusted-by-container">
-      <h2>Trusted By</h2>
-      <div className="logo-container">
-       <img src="https://job-portal-gs-client.vercel.app/assets/microsoft_logo-BrRPO5-k.svg" alt="Microsoft" />
+    <div className="trusted-by-section">
+      <div className="trusted-container">
+        <h2 className="trusted-title">Trusted By</h2>
+        <div className="trusted-logos">
+          <img src="https://job-portal-gs-client.vercel.app/assets/microsoft_logo-BrRPO5-k.svg" alt="Microsoft" />
         <img src="https://job-portal-gs-client.vercel.app/assets/walmart_logo-BNc35QHy.svg" alt="Walmart" />
-        <img src="https://job-portal-gs-client.vercel.app/assets/samsung_logo-BxQL65ad.png" alt="Other Logo" /> 
+        <img src="https://job-portal-gs-client.vercel.app/assets/samsung_logo-BxQL65ad.png" alt="Other Logo" />
+      
+        
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default TrustedBy;
