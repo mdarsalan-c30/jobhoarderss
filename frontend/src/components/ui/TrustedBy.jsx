6966f5c2b0f116d8e225a4474e6ca1b36b585397
mdarsalan-c30 <105696006+mdarsalan-c30@users.noreply.github.com>
2025-05-07 12:55:00 +0530

    Create TrustedBy.jsx

---
+++ frontend/src/components/ui/TrustedBy.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './TrustedBy.css';  // <-- we'll create this CSS file next
+
+const TrustedBy = () => {
+  return (
+    <div className="trusted-by-section">
+      <div className="trusted-container">
+        <p className="trusted-title">Trusted by</p>
+        <div className="trusted-logos">
+         <img src="https://job-portal-gs-client.vercel.app/assets/microsoft_logo-BrRPO5-k.svg" alt="Microsoft" />
+        <img src="https://job-portal-gs-client.vercel.app/assets/walmart_logo-BNc35QHy.svg" alt="Walmart" />
+        <img src="https://job-portal-gs-client.vercel.app/assets/samsung_logo-BxQL65ad.png" alt="Other Logo" />
+        <img src="https://job-portal-gs-client.vercel.app/assets/microsoft_logo-BrRPO5-k.svg" alt="Microsoft" />
+        <img src="https://job-portal-gs-client.vercel.app/assets/walmart_logo-BNc35QHy.svg" alt="Walmart" />
+        <img src="https://job-portal-gs-client.vercel.app/assets/samsung_logo-BxQL65ad.png" alt="Other Logo" />
+        
+          {/* Add more logos as needed */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
