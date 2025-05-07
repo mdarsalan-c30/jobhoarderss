632e5a497c4cbf1d3de24f65e38e5b9d4557ab7b
mdarsalan-c30 <105696006+mdarsalan-c30@users.noreply.github.com>
2025-05-07 13:44:48 +0530

    Update TrustedBy.jsx

--- frontend/src/components/ui/TrustedBy.jsx
+++ frontend/src/components/ui/TrustedBy.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TrustedBy } from '../util/trustedby;    // <-- we'll create this CSS file next
+import { TrustedBy } from '../util/trustedby;
 
 const TrustedBy = () => {
   return (
