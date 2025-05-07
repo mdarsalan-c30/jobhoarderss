86a3ac763f025edfbd9bf52c999654d6d6166f67
mdarsalan-c30 <105696006+mdarsalan-c30@users.noreply.github.com>
2025-05-07 14:04:44 +0530

    Update TrustedBy.jsx

--- frontend/src/components/TrustedBy.jsx
+++ frontend/src/components/TrustedBy.jsx
@@ -8,11 +8,7 @@ const TrustedBy = () => {
       <div className="logo-container">
        <img src="https://job-portal-gs-client.vercel.app/assets/microsoft_logo-BrRPO5-k.svg" alt="Microsoft" />
         <img src="https://job-portal-gs-client.vercel.app/assets/walmart_logo-BNc35QHy.svg" alt="Walmart" />
-        <img src="https://job-portal-gs-client.vercel.app/assets/samsung_logo-BxQL65ad.png" alt="Other Logo" />
-        <img src="https://job-portal-gs-client.vercel.app/assets/microsoft_logo-BrRPO5-k.svg" alt="Microsoft" />
-        <img src="https://job-portal-gs-client.vercel.app/assets/walmart_logo-BNc35QHy.svg" alt="Walmart" />
-        <img src="https://job-portal-gs-client.vercel.app/assets/samsung_logo-BxQL65ad.png" alt="Other Logo" />
-        
+        <img src="https://job-portal-gs-client.vercel.app/assets/samsung_logo-BxQL65ad.png" alt="Other Logo" /> 
       </div>
     </div>
   );
